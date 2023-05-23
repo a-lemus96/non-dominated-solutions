6f8307abe95947e4caf5c1cacc8797480e0a1dac
a-lemus96 <alejandro.lemus@cimat.mx>
2023-05-22 18:20:47 -0600

    feat: Add 2D and 3D n-dominated generating fn
    Add function to generate a set of 2D or 3D points where a particular
    amount of points represents a non-dominated solution.

--- ndsols.py
+++ ndsols.py
@@ -11,7 +11,6 @@ def naive_ndset(sols: ndarray):
     Returns:
         ndsols: set of non-dominated solutions"""
     n, d = sols.shape # retrieve number of sols and dimension
-    print(n, d)
     ndsols = []
     for i in range(n):
         for j in range(n):
@@ -23,3 +22,33 @@ def naive_ndset(sols: ndarray):
             ndsols.append(sols[i])
 
     return np.array(ndsols)
+
+def generate_set(n: int, d: int, r: float, ratio: float = 0.) -> ndarray:
+    """Generates a set of either 2D or 3D points among which a number is consi-
+    dered to be a non-dominated solution. Reference Pareto front is considered
+    to be the surface of the hypersphere (x - r)^2 + (y - r)^2 + (z - r)^2 = r^2
+    where x, y, z in [0, r].
+    ----------------------------------------------------------------------------
+    Args:
+        n: number of desired points
+        d: dimension of datapoints
+        r: radius of hypersphere
+        ratio: ratio of dominated solutions to the total number of solutions
+    Returns:
+        points: (n, d)-shape ndarray containing the desired number of non-domi-
+                nated solutions"""
+    points = np.random.normal(size=(n, d)) # sample gaussian
+    # normalize each point
+    points /= np.linalg.norm(points, axis=-1)[:, np.newaxis]
+    # fold points such that all points have negative coordinates
+    points[points > 0] *= -1
+    # draw sample of points to convert into non-dominated
+    k = int(n*ratio) # number of contaminated points
+    idxs = np.random.choice(len(points), size=k, replace=False)
+    factors = np.random.rand(k) # factors to shrink selected points
+    points[idxs] *= factors[:, np.newaxis]
+    # scale samples and translate them
+    points *= r
+    points += r
+    
+    return points
--- test.py
+++ test.py
@@ -3,7 +3,10 @@ import argparse
 
 # thrid-party modules
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # custom modules
 import ndsols
@@ -27,3 +30,19 @@ non_dominated = ndsols.naive_ndset(points)
 plt.scatter(points[:, 0], points[:, 1])
 plt.scatter(non_dominated[:, 0], non_dominated[:, 1])
 plt.savefig("nd_naive.png")
+plt.close()
+
+'''plt.figure(figsize=(8,8))
+points = ndsols.generate_set(50, 2, 9, 0.5)
+plt.scatter(points[:, 0], points[:, 1])
+plt.savefig("2d.png")
+plt.close()'''
+
+points = ndsols.generate_set(500, 3, 9, 0.5)
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1, projection='3d')
+ax.scatter(points[:, 0], points[:, 1], points[:, 2])
+points = ndsols.naive_ndset(points)
+ax.scatter(points[:, 0], points[:, 1], points[:, 2])
+plt.show()
+plt.savefig("3d.png")
